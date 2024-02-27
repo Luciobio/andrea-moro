018fa4c3771f7b3ea3bdf3d16b1e8a0da00d1e9c
Luciano Pardo <pardobio@gmail.com>
2024-02-27 02:57:07 -0300

    Update about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,10 +17,10 @@ export const About = () => {
                     <Image className="align-center w-full h-full lg:w-9/12 rounded-full" src={aboutSelfie} alt={""} />
                 </div>
                 <div className="flex flex-col justify-center w/auto lg:w-1/3 md:w-2/3 sm:w-2/3">
-                    <Image className="-ml-20 lg:flex md:hidden sm:hidden hidden" src={hola} alt={"Hola"} />
+                    <Image className="-ml-24 lg:flex md:hidden sm:hidden hidden mt-4" src={hola} alt={"Hola"} height={90}/>
                     <h2 className="font-extralight text-xl lg:text-3xl md:text-2xl sm:text-xl py-3 lg:mx-1 mx-auto" >SOY ANDREA</h2>
-                    <p className="w-4/5 font-light mx-auto mb-2 text-sm lg:text-base md:text-sm sm:text-sm " >Artista y guía en el fascinante viaje a la creatividad.
-                        Amante de la flores, la naturaleza y la decoración. 
+                    <p className="w-4/5 font-light mx-auto text-sm lg:text-sm md:text-sm sm:text-sm mb-4" >Artista y guía en el fascinante viaje a la creatividad.
+                        Amante de la flores, la naturaleza y la decoración.
                         A través de mis cursos , te invito a explorar el arte de crear flores con tus propias manos
                         Acompáñame en este trayecto donde la creatividad transformará espacios y corazones.
                         <br />Bienvenida a mi mundo vibrante y lleno de inspiración!!!</p>
