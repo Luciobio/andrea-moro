6662299c0ce8be421155a35c57ea6bd809af24ee
Luciano Pardo <pardobio@gmail.com>
2024-03-04 02:46:46 -0300

    Navbar update responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,76 +1,94 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Image from "next/image";
 import andreaLogo from "../../public/images/logo_svg.svg"
 import { CiSearch, CiShoppingCart, CiUser } from 'react-icons/ci';
 import Link from 'next/link';
 
 export const Navbar = () => {
+  const [nav, setNav] = useState('hidden');
+
+  const openNav = () => {
+    nav == 'hidden' ? setNav('flex') : setNav('hidden')
+    console.log()
+  }
+
+
+
   return (
     <div>
-      {/* <!-- navbar --> */}
+      {/* Heading */}
       <nav className="flex justify-center bg-vanilla">
         <Link href={'/'}>
           <Image className="py-5" src={andreaLogo} alt={"Logo"} width={150} height={150} priority={false} />
         </Link>
       </nav>
 
+      {/* NavBar */}
       <nav className="flex justify-center items-center align-middle bg-vanilla">
-        <div className="px-5 xl:px-6 py-3 flex w-10/12 justify-center items-center align-middle">
 
-          <div id='search' className='w-3/12 flex justify-start align-middle'>
-            <a className="flex items-center hover:text-marroncalido" href="#">
+        <div className="py-3 flex justify-between lg:justify-center xl:justify-center items-center align-middle w-full xl:w-10/12 lg:w-10/12">
+
+          {/* Sección Izquierda: Burguer y Busqueda */}
+          <div id='search' className='w-3/12 flex ml-2 justify-start align-middle'>
+
+            <div className="mx-2 xl:hidden lg:hidden md:hidden flex cursor-pointer" onClick={() => openNav()}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </div>
+
+            <Link className="flex mx-2 items-center hover:text-marroncalido" href="/#cursos">
               <CiSearch className='text-2xl' strokeWidth="1" />
-            </a>
+            </Link>
+
           </div>
 
-          <div id='menu' className='w-6/12 flex justify-center align-middle'>
+          {/* Sección medio: Navegación */}
+          <div id='menu' className='w-6/12 justify-center align-middle'>
             {/* <!-- Nav Links --> */}
-            <ul className="hidden md:flex px-2 font-semibold font-heading space-x-16 xl:flex justify-between">
-              <li><Link className="hover:text-marroncalido" href="/#sobre-mi">Sobre Mi</Link></li>
+            <ul className='hidden xl:flex lg:flex md:flex justify-between px-2 font-semibold'>
+              <li><Link className="hover:text-marroncalido truncate" href="/#sobre-mi">Sobre Mi</Link></li>
               <li><Link className="hover:text-marroncalido" href="/#cursos">Cursos</Link></li>
               <li><Link className="hover:text-marroncalido" href="/#galeria">Galería</Link></li>
               <li><Link className="hover:text-marroncalido" href="/#contacto">Contacto</Link></li>
             </ul>
           </div>
 
+          {/* Sección derecha: Carrito y Usuario */}
           <div id='cart-login' className='w-3/12 flex justify-end align-middle'>
-            {/* <!-- Header Icons --> */}
-            <div className="hidden xl:flex space-x-5">
-              <a className="flex items-center hover:text-marroncalido" href="#">
-                <CiShoppingCart className='text-2xl' strokeWidth="1" />
-                {/* "Carrito" */}
-                <span className="flex absolute -mt-5 ml-4">
-                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-marroncalido opacity-75"></span>
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-marroncalido"></span></span>
+
+            <div className="hidden mr-2">
+
+              <a className="flex mx-2 items-center hover:text-marroncalido" href="#">
+                <CiUser className='text-2xl' strokeWidth="1" />
               </a>
 
-              {/* <!-- Sign In / Register      --> */}
-              <a className="flex items-center hover:text-marroncalido" href="#">
-                <CiUser className='text-2xl' strokeWidth="1" />
+              <a className="flex mx-2 items-center hover:text-marroncalido" href="#">
+                <CiShoppingCart className='text-2xl' strokeWidth="1" />
+                <span className="flex absolute -mt-5 ml-4">
+                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-marroncalido opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-3 w-3 bg-marroncalido"></span>
+                </span>
               </a>
 
             </div>
           </div>
 
         </div>
-        {/* <!-- Responsive navbar --> */}
-        <a className="xl:hidden flex mr-6 items-center hover:text-marroncalido" href="#">
-          <CiShoppingCart className='text-2xl' strokeWidth="1" />
-          <span className="flex absolute -mt-5 ml-4">
-            <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-marroncalido opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-marroncalido"></span>
-          </span>
-        </a>
-        <a className="flex items-center hover:text-marroncalido xl:hidden" href="#">
-          <CiUser className='text-2xl' strokeWidth="1" />
-        </a>
-        {/* <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-          "Menu"
-      </a> */}
       </nav>
+
+      <div id='menuMobile' className={ `${nav} xl:hidden lg:hidden md:hidden w-full justify-center align-middle py-2 bg-vanilla`}>
+        {/* <!-- Nav Links --> */}
+        <ul className='flex flex-col justify-center items-center font-semibold w-full h-auto space-y-1'>{/*  'hidden xl:flex lg:flex justify-between px-2 font-semibold' */}
+          <li><Link className="hover:text-marroncalido truncate" href="/#sobre-mi">Sobre Mi</Link></li>
+          <li><Link className="hover:text-marroncalido" href="/#cursos">Cursos</Link></li>
+          <li><Link className="hover:text-marroncalido" href="/#galeria">Galería</Link></li>
+          <li><Link className="hover:text-marroncalido" href="/#contacto">Contacto</Link></li>
+        </ul>
+      </div>
+
+
 
     </div>
   )
