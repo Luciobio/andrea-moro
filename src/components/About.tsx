0f9ffecdaaac1ec16b05b00645da6ceece046571
Juan Maldini <juan.maldini@outlook.com>
2026-05-31 18:43:44 -0300

    main foto intro

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import aboutbg from "../../public/images/Fondo2.webp";
-import aboutSelfie from "../../public/images/About_selfie_a.webp";
+import aboutSelfie from "../../public/images/main.jpeg";
 import aboutFlower from "../../public/images/About_flower_a.webp";
 import hola from "../../public/images/Hola.png";
 
@@ -14,16 +14,20 @@ export const About = () => {
         >
             <div className="flex flex-col items-center justify-center size-4/5 xl:lg:flex-row xl:lg:size-9/12 bg-grisoscuro">
                 <div className="flex items-center justify-center w-1/3">
-                    <Image draggable='false' className="my-4 align-center w-full h-full xl:lg:w-9/12 rounded-full overflow-hidden" src={aboutSelfie} alt={""} />
+                    <Image draggable='false' className="my-4 align-center w-full h-full xl:lg:w-9/12  overflow-hidden" src={aboutSelfie} alt={""} />
                 </div>
                 <div className="flex flex-col justify-center w/auto lg:w-1/3 md:w-2/3 sm:w-2/3">
                     <Image draggable='false' className="-ml-24 lg:flex md:hidden sm:hidden hidden mt-4" src={hola} alt={"Hola"} height={90}/>
                     <h2 className="font-extralight text-xl lg:text-3xl md:text-2xl sm:text-xl py-3 lg:mx-1 mx-auto" >SOY ANDREA</h2>
-                    <p className="w-4/5 font-light mx-auto text-sm lg:md:sm:text-sm mb-8 overflow-hidden" >Artista y guía en el fascinante viaje a la creatividad.
-                        Amante de las flores, la naturaleza y la decoración.
-                        A través de mis cursos , te invito a explorar el arte de crear flores con tus propias manos.
-                        Acompáñame en este trayecto donde la creatividad transformará espacios y corazones.
-                        <br />Bienvenida a mi mundo vibrante y lleno de inspiración!!!</p>
+                    <p className="w-4/5 font-light mx-auto text-sm lg:md:sm:text-sm mb-8 overflow-hidden">Formadora en arte floral eterno.
+                    
+                    <br />Enseño a crear flores realistas con método, técnica y alma. Mi propósito es ayudarte a desarrollar habilidades artísticas y lograr piezas que reflejen la belleza de la naturaleza con un alto nivel de realismo.
+                    
+                    <br />A través de mis cursos, comparto los conocimientos, procesos y secretos que he perfeccionado durante años para que puedas crear con confianza y alcanzar resultados extraordinarios.
+                    
+                    </p>
+
+                    
                 </div>
                 <Image  draggable='false' className="h-full w-1/3 lg:flex md:hidden sm:hidden hidden aspect-[3/4] object-cover" src={aboutFlower} alt={"Magnolia"} />
             </div>
