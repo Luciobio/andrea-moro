4f75c48e8b0625492d1c0af5650aef221a70dba4
Juan Maldini <juan.maldini@outlook.com>
2026-06-08 17:51:35 -0300

    foto

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
-import andreadesk from '../../public/images/Andrea_desk.webp'
+import cursosPresenciales from '../../public/images/Cursos_presenciales.webp'
 import { IoLogoWhatsapp } from 'react-icons/io'
 import Link from 'next/link'
 
 export const Contact = () => {
   return (
     <div id='contacto' className='lg:flex lg:flex-row md:flex md:flex-row flex flex-col h-3/5 bg-grisoscuro'>
-      <Image id='pic_andrea_desk' className='object-cover size-full lg:size-3/5 md:size-3/5 sm:size-4/5' src={andreadesk} alt={'Andrea escritorio'} />
-      <div className='flex flex-col items-center justify-center align-middle bg-grisclaro h-3/6 my-auto p-10 lg:-ml-32 md:-ml-32'>
+      <div className='flex flex-col items-center justify-center align-middle bg-grisclaro h-3/6 my-auto p-10 lg:-mr-32 md:-mr-32 z-10'>
         <h2 className='text-3xl text-center font-bold'>Cursos en vivo presenciales</h2>
         <span className='w-8/12 my-10'>
           <p className='text-xl font-light text-center'> Ponete en contacto conmigo para coordinar la posibilidad de organizar un curso presencial en tu localidad.</p>
@@ -21,6 +20,7 @@ export const Contact = () => {
         </div>
 
       </div>
+      <Image id='pic_andrea_desk' className='object-cover size-full lg:size-3/5 md:size-3/5 sm:size-4/5' src={cursosPresenciales} alt={'Cursos presenciales'} />
     </div>
   )
 }
